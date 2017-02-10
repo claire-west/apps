c80dc233c705b4aff6395464c60f8cd230dff990
Isaac West <bokthaku@gmail.com>
2017-02-09 21:03:24 -0500

    Added indexer, getProp, binarySearch, and D&D json (#3)

--- shared/js/ajaxLoader.js
+++ shared/js/ajaxLoader.js
@@ -2,9 +2,13 @@
     window.dynCore.declare('ajaxLoader',
         window.dynCore.loadTemplate('ajaxLoader', '../shared/html/ajaxLoader.html'),
         function() {
-            return function(promise, $container) {
+            return function(promise, $container, text) {
                 var $elements = $container.find('*').filter(':visible').hide();
-                var $ajaxLoader = window.dynCore.makeFragment('ajaxLoader').prependTo($container);
+                var $ajaxLoader = window.dynCore.makeFragment('ajaxLoader', {
+                    'span': {
+                        text: (text || 'Loading') + ' '
+                    }
+                }).prependTo($container);
                 return promise.always(function() {
                     $ajaxLoader.remove();
                     $elements.show();
